63c4ed1180b9646b664c8b415e05911d332ccc2d
dejuaun <dejuaun@gmail.com>
2024-01-10 16:25:14 +0000

    logic update

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 //Setting up the variables for later use
-const userScore = 0;
-const compScore = 0;
+let userScore = 0;
+let compScore = 0;
 const userScore_span = document.getElementById('user-score');
 const compScore_span = document.getElementById('comp-score');
-const scoreBoard_div = document.querySelector('.scoreBaord');
-const result_div = document.querySelector('.result');
+const scoreBoard_div = document.querySelector('.scoreBoard');
+const result_p = document.querySelector('.result p');
 const rock_div = document.getElementById('r');
 const paper_div = document.getElementById('p');
 const scissors_div = document.getElementById('s');
@@ -15,9 +15,48 @@ function getCompChoice() {
     randomNumber = (Math.floor(Math.random() * 3));
     return choices[randomNumber]
 }
-//Takes and compares user coice against comp choice and returns the result
+//This converts the user and computer choice to readable words
+function convertToWord(letter) {
+
+    if (letter === 'r') return "Rock";
+    if (letter === 'p') return 'Paper';
+    return 'Scissors';
+}
+
+//Adds The Functionality For When The User Wins
+function win(userChoice, compChoice) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    compScore_span.innerHTML = compScore;
+    result_p.innerHTML = `${convertToWord(userChoice)} beats ${convertToWord(compChoice)}. You Win!`;
+}
+function lose() {
+    compScore;
+}
+function draw() {
+    console.log('draw');
+}
+
+//Takes and compares user choice against comp choice and returns the result
 function game(userChoice) {
-    const CompChoice = getCompChoice();
+    const compChoice = getCompChoice();
+    switch (userChoice + compChoice) {
+        case 'rs':
+        case 'pr':
+        case 'sp':
+            win(userChoice, compChoice);
+            break;
+        case 'rp':
+        case 'ps':
+        case 'sr':
+            lose(userChoice, compChoice);
+            break;
+        case 'rr':
+        case 'ps':
+        case 'ss':
+            draw(userChoice, compChoice);
+            break;
+    }
 }
 
 function main() {
@@ -35,5 +74,3 @@ function main() {
 }
 
 main();
-
-jlnjn
